467eadced1108f2b020916d5cba93b646299e7cb
Resul <68106627+SamedovResul@users.noreply.github.com>
2021-03-22 12:47:42 +0400

    test-project

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const text = document.querySelectorAll('.text')
+const question = document.querySelector('.question')
+const questionArray = ['Bunlardan hansı ətyeyən bitkidir?']
+question.innerHTML = questionArray
+const answerArray = {
+    One:[ 'Milçəkqapan', 'Qızılgül','Palıd' ,'Pambıq'],
+    Two:[ 'Yonca', 'Şehçiçəyi','Zanbaq' ,'Çaytikanı'],
+    Three:[ 'Çobanyastığı', 'Qovaq ', 'Meymun fincanı' ,'Dəvədabanı'],
+}
+
+
+const questions =   Math.floor(Math.random() * 3);
+console.log()
+let answer = 0
+let trueAnswer;
+for (let i = 0; i < text.length; i++) {
+    console.log(text[i])
+    if(0 == questions){
+        text[0].innerHTML = answerArray.One[0]
+        text[1].innerHTML = answerArray.One[1]
+        text[2].innerHTML = answerArray.One[2]
+        text[3].innerHTML = answerArray.One[3]
+        trueAnswer = "Milçəkqapan" 
+    }else if(1 == questions){
+        text[0].innerHTML = answerArray.Two[0]
+        text[1].innerHTML = answerArray.Two[1]
+        text[2].innerHTML = answerArray.Two[2]
+        text[3].innerHTML = answerArray.Two[3]
+        trueAnswer = "Şehçiçəyi" 
+    }else if(2 == questions){
+        text[0].innerHTML = answerArray.Three[0]
+        text[1].innerHTML = answerArray.Three[1]
+        text[2].innerHTML = answerArray.Three[2]
+        text[3].innerHTML = answerArray.Three[3]
+        trueAnswer = 'Meymun fincanı'
+    }
+}
+let value;
+trueAnswercss = 
+$(document).ready(function () {
+    $(".quiz-option").click(function () {
+        $(".quiz-option").css('border', 'none')
+        value = $(this).children("label").text()
+        trueAnswercss = $(this)
+        $(this).css("border", '1px solid black')
+        console.log(value);
+    });
+    $(".click").click(function(){
+        if (value == trueAnswer) {
+            console.log(true)
+            $(".click").fadeOut()
+            $(".warning").fadeOut(0.1);
+            $(".gamelink").fadeIn(1000);
+            trueAnswercss.css("background-color", "green")
+            console.log(trueAnswer)
+        } else {
+            $(".gamelink").fadeOut();
+            $(".warning").fadeIn();
+        }
+    })
+});
